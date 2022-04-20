9007b649d1164b94704e145b61714a2286f22fef
Mroknsharifi <90482394+Mroknsharifi@users.noreply.github.com>
2022-04-20 06:09:35 -0400

    Add files via upload

---
+++ layouts/footer.php
@@ -0,0 +1,61 @@
+</div>
+
+<footer class="footer-distributed">
+
+        <div class="footer-left">
+
+            <img src="Booksy-logos_white.png">
+            <p class="footer-links">
+                <a href="#" class="link-1">Home</a>
+
+                <a href="#">COLLECTION</a>
+
+                <a href="#">STORE</a>
+
+                <a href="#">BOOK DETAIL</a>
+
+            </p>
+        </div>
+
+        <div class="footer-center">
+
+            <div>
+                <i class="fa fa-map-marker"></i>
+                <p><span>108 University Ave. East. </span> WATERLOO, ON N2J 2W2 </p>
+            </div>
+
+            <div>
+                <i class="fa fa-phone"></i>
+                <p>+1.555.555.5555</p>
+            </div>
+
+            <div>
+                <i class="fa fa-envelope"></i>
+                <p><a href="booksy@company.com">booksy@company.com</a></p>
+            </div>
+
+        </div>
+
+        <div class="footer-right">
+
+            <p class="footer-company-contact">
+                <span>Contact Info</span> Asheshkalanigha1242@conestogac.on.ca
+                <br>Mroknsharifi9708@conestogac.on.ca
+                <br>Horen3591@conestogac.on.ca
+
+
+            </p>
+
+
+
+        </div>
+
+</footer>
+
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>    
+<script src="assets/js/cart.js"></script>
+</body>
+
+</html>
---
+++ layouts/header.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Checkout Page</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="<?php echo (isset($metaDesc)?$metaDesc:'Demo PHP Shopping Cart')?>">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="stylebody.css">
+
+  
+    <title><?php echo (isset($pageTitle)?$pageTitle:'PHP Shopping Cart')?></title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+</head>
+<body>
+
+    <div class="search">
+        <div class="logo"><img src="Booksy-logo.png"></div>
+        <input type="search" id="site-search" name="q"><button>Search</button>
+    </div>
+
+    <div id="wrap">
+        <div id="nav">
+            <ul>
+                <li><a href="#">HOME</a></li>
+                <li><a href="#">COLLECTION</a></li>
+                <li><a href="#">STORE</a></li>
+                <li><a href="#">BOOK DETAIL</a></li>
+            </ul>
+        </div>
+    </div>
+
+   <div class="container">
+        <div class="row mt-2 mb-2">
+            <div class="col-md-12 col-xs-12">
+                <h1>
+                  
+                </h1>
+            </div>
+        </div>
+        
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0"">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php">Store <span class="sr-only">(current)</span></a>
+                </li>
+            </ul>
+            <div class="form-inline my-2 my-lg-0">
+                <a href="cart.php" style="color:#ffffff">
+                    <i class="bi bi-cart4" style="font-size:30px;"></i>
+                    <?php 
+                        echo (isset($_SESSION['cart_items']) && count($_SESSION['cart_items'])) > 0 ? count($_SESSION['cart_items']):'';
+                    ?>
+                </a>
+            </div>
+            
+        </nav>
