6362f3742fefa8b67f1174014081955fca690dc5
Mroknsharifi <90482394+Mroknsharifi@users.noreply.github.com>
2022-04-20 15:21:13 -0400

    Add files via upload

--- layouts/header.php
+++ layouts/header.php
@@ -7,32 +7,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="description" content="<?php echo (isset($metaDesc)?$metaDesc:'Demo PHP Shopping Cart')?>">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="stylesheet" href="assets/css/stylebody.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="stylebody.css">
+
+  
+    <title><?php echo (isset($pageTitle)?$pageTitle:'PHP Shopping Cart')?></title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
-  
-    <title><?php echo (isset($pageTitle)?$pageTitle:'PHP Shopping Cart')?></title>
-
 </head>
 <body>
 
     <div class="search">
-        <div class="logo"><img src="assets/images/Booksy-logo.png" /></div>
-        <div>
-            <form action="bookList.php" method="get">
-                <input type="search" id="site-search" name="search" /><button>Search</button>
-            </form>
-        </div>
+        <div class="logo"><img src="Booksy-logo.png"></div>
+        <input type="search" id="site-search" name="q"><button>Search</button>
     </div>
 
     <div id="wrap">
         <div id="nav">
             <ul>
-                <li><a href=".">HOME</a></li>
-                <li><a href="#">COLLECTION</a></li>
-                <li><a href="#">STORE</a></li>
+            <li><a href=".">HOME</a></li>
+                
+                <li><a href="store.php">STORE</a></li>
                 <li><a href="bookList.php">BOOK LIST</a></li>
                 <li><a href="login.php">LOGIN</a></li>
                 <li><a href="signup.php">SIGN UP</a></li>
@@ -50,7 +46,7 @@
         </div>
         
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0"">
                 <li class="nav-item active">
                     <a class="nav-link" href="index.php">Store <span class="sr-only">(current)</span></a>
                 </li>
